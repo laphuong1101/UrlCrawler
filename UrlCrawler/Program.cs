4f64079416c776791e1362145bd1bd876296b19f
phuonglvd00631 <phuonglvd00631@fpt.edu.vn>
2021-01-15 19:55:01 +0700

    bbb

---
+++ UrlCrawler/Program.cs
@@ -0,0 +1,61 @@
+﻿using HtmlAgilityPack;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> MyUrls = new HashSet<string>();
+
+            HtmlWeb web = new HtmlWeb()
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8  //Set UTF8 để hiển thị tiếng Việt
+            };
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load("https://vnexpress.net/suc-khoe/dinh-duong");
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+
+                if (att.Value.EndsWith(".html"))
+                {
+                    MyUrls.Add(att.Value);
+                }
+            }
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "amqhelloworld",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    foreach (var item in MyUrls)
+                    {
+                        string message = item;
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "amqhelloworld",
+                                             basicProperties: null,
+                                             body: body);
+                    }
+
+                }
+            }
+        }
+    }
+}
